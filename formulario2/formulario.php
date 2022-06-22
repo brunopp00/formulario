087203289c702e2320311bdd5e52c39e610dcda0
Bruno Frohlich <68931062+brunopp00@users.noreply.github.com>
2022-06-22 12:25:19 -0300

    asdas

--- formulario2/formulario.php
+++ formulario2/formulario.php
@@ -1,20 +1,33 @@
 <?php
 if (isset($_POST['submit'])) {
-  print_r('Nome: ' . $_POST['nome']);
-  print_r('<br>');
-  print_r('Email: ' . $_POST['email']);
-  print_r('<br>');
-  print_r('Telefone: ' . $_POST['telefone']);
-  print_r('<br>');
-  print_r('Sexo: ' . $_POST['genero']);
-  print_r('<br>');
-  print_r('Data de Nascimento:' . $_POST['data_nascimento']);
-  print_r('<br>');
-  print_r('Cidade: ' . $_POST['cidade']);
-  print_r('<br>');
-  print_r('Estado: ' . $_POST['estado']);
-  print_r('<br>');
-  print_r('Endereço: ' . $_POST['endereco']);
+  //print_r('Nome: ' . $_POST['nome']);
+  //print_r('<br>');
+  //print_r('Email: ' . $_POST['email']);
+  //print_r('<br>');
+  //print_r('Telefone: ' . $_POST['telefone']);
+  //print_r('<br>');
+  //print_r('Sexo: ' . $_POST['genero']);
+  //print_r('<br>');
+  //print_r('Data de Nascimento:' . $_POST['data_nascimento']);
+  //print_r('<br>');
+  //print_r('Cidade: ' . $_POST['cidade']);
+  //print_r('<br>');
+  //print_r('Estado: ' . $_POST['estado']);
+  //print_r('<br>');
+  //print_r('Endereço: ' . $_POST['endereco']);
+
+  include_once('config.php');
+
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $telefone = $_POST['telefone'];
+  $sexo = $_POST['genero'];
+  $data_nasc =  $_POST['data_nascimento'];
+  $cidade = $_POST['cidade'];
+  $estado = $_POST['estado'];
+  $endereco = $_POST['endereco'];
+
+  $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,email,telefone,sexo,data_nasc,cidade,estado,endereco) VALUES ('$nome','$email','$telefone','$sexo','$data_nasc','$cidade','$estado','$endereco')");
 }
 ?>
 <!DOCTYPE html>
@@ -132,13 +145,13 @@ if (isset($_POST['submit'])) {
           <label for="telefone" class="labelInput">Telefone</label>
         </div>
         <p>Sexo:</p>
-        <input type="radio" name=genero" id="feminino" value="feminino" required>
+        <input type="radio" name="genero" id="feminino" value="feminino" required>
         <label for="feminino">Feminino</label>
         <br />
-        <input type="radio" name=genero" id="masculino" value="masculino" required>
+        <input type="radio" name="genero" id="masculino" value="masculino" required>
         <label for="masculino">Masculino</label>
         <br />
-        <input type="radio" name=genero" id="outro" value="outro" required>
+        <input type="radio" name="genero" id="outro" value="outro" required>
         <label for="outro">Outro</label>
         <br /><br />
         <label for="data_nascimento"><b>Data de Nascimento:</b></label>
